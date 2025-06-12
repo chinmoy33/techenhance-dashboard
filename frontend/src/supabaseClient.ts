573acbf533f3a99bcfe7e9b36a6a55768eb834da
chinmoy <chinmoysharma2003@gmail.com>
2025-06-12 21:19:27 +0530

    migrated to another supabase organisation

--- frontend/src/supabaseClient.ts
+++ frontend/src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://zhbjxshlsvxxggpyrygb.supabase.co'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL; // ✅ Vite way
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY; // ✅ Vite way
 export const supabase = createClient(supabaseUrl, 
                                     supabaseKey,
